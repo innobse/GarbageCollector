3d023f8ec8f368240135d9f24875fc3cd3d39e70
Yury Penkov <bsesie71@gmail.com>
2017-02-22 11:39:10 +0300

    Initial commit

---
+++ src/main/Main.java
@@ -0,0 +1,41 @@
+package main;
+
+import java.io.RandomAccessFile;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    private static final int LOOK_COUNTER = 10_000_000;
+    static List<Object> cache = new LinkedList<>();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            switch (sc.next()){
+                case "collectable":
+                    System.out.println("collectable");
+                    createBigObject();
+                    break;
+                case "clickable":
+                    System.out.println("clickable");
+                    cache.add(createBigObject());
+                    break;
+                default: break;
+            }
+        }
+
+    }
+
+    public static List<Object> createBigObject(){
+        Random rand = new Random();
+        List<Object> result = new LinkedList<>();
+        for (int i = 0; i < LOOK_COUNTER; i++) {
+            String tmp = "anystring" + rand.nextInt();
+            result.add(tmp);
+            System.out.println(tmp);
+        }
+        return result;
+    }
+}
